796762c00cc9d3891cb5503734694c983509464d
nlnadialia <nlnadialigia@gmail.com>
2020-10-14 01:16:53 -0300

    class 2 until images upload

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,43 +1,13 @@
 import express from 'express'
-import {getRepository} from 'typeorm'
+
 
 import './database/connection'
-import Orphanage from './models/orphanage'
+
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.post('/orphanages', async (request, response)=>{
-    console.log(request.body);
-    
-    const {
-        id,
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body
-
-    const orphanagesRepository = getRepository(Orphanage)
-    const orphanage = orphanagesRepository.create({
-        id,
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    })
-
-    await orphanagesRepository.save(orphanage)
-
-    return response.json({ message: 'Hello Word' })
-    
-})
+app.use(routes)
 
 app.listen(3333, ()=>{console.log('SERVER IS RUNNING')})
